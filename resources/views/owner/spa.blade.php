eded560ded1f683128c7af98fe1a3558171ae889
Rodfil T. Tayong <rttayong@cli.lan>
2023-09-28 17:21:20 +0800

    search,add and modal features

--- resources/views/owner/spa.blade.php
+++ resources/views/owner/spa.blade.php
@@ -5,7 +5,19 @@
     <div class="col-lg-12 grid-margin stretch-card">
         <div class="card">
             <div class="card-body">
+            <div class="d-flex justify-content-between align-items-center">
                 <h4 class="card-title">Manage Spa</h4>
+                <button type="button" 
+                    data-bs-toggle="modal" 
+                    data-bs-target="#exampleModal"
+                    class="btn btn-success mb-3"
+                 >Add
+                </button>
+            </div>
+                <div class="input-group">
+                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                    <button type="button" class="btn btn-outline-primary">search</button>
+                </div>
                 <div class="table-responsive">
                 <table class="table table-striped">
                     <thead>
@@ -47,5 +59,24 @@
             </div>
         </div>
     </div>
+</div>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+            <i class="mdi mdi-close"></i> mdi mdi-close
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
