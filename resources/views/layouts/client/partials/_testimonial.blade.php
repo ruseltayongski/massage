ae575d3f522d8b48135a5159962a23e80ddd85a6
Rodfil T. Tayong <rttayong@cli.lan>
2023-11-22 15:13:07 +0800

    PushUpdates

--- resources/views/layouts/client/partials/_testimonial.blade.php
+++ resources/views/layouts/client/partials/_testimonial.blade.php
@@ -59,7 +59,7 @@
     
 </style>
 <!-- Testimonial Start -->
-@if(request()->route()->getName() === '/' || request()->route()->getName() === 'client.services')
+@if(request()->route()->getName() === '/')
 <div class="container-fluid py-5">
     <div class="container py-5">
         <div class="row align-items-center">
@@ -67,8 +67,23 @@
                 <img class="img-fluid w-100" src="{{ asset('client/img/testimonial.jpg') }}" alt="">
             </div>
             <div class="col-lg-6">
-                <h6 class="d-inline-block text-primary text-uppercase bg-light py-1 px-2">Feedback</h6>
+                <h6 class="d-inline-block text-primary text-uppercase bg-light py-1 px-2">Testimonials</h6>
                 <h1 class="mb-4">What Our Clients Say!</h1>
+                <div class="owl-carousel testimonial-carousel">
+                  
+                    @foreach($testimonials as $testimonial)
+                    <div class="position-relative">
+                        <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0;"></i>
+                        <div class="d-flex align-items-center mb-3">
+                            <img class="img-fluid rounded-circle" src="{{ asset('fileupload/client/profile').'/'.$testimonial->user_profile }}" style="width: 60px; height: 60px;" alt="">
+                            <div class="ml-3">
+                                <h6 class="text-uppercase">{{ $testimonial->name }}</h6>
+                                <span>{{ ucfirst(strtolower($testimonial->roles)) }}</span>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
@@ -147,6 +162,19 @@
         </div>
     </div>
 </div>
-
+@elseif(request()->route()->getName() === 'client.services')
+<div class="container-fluid py-5">
+    <div class="container py-5">
+        <div class="row align-items-center">
+            <div class="col-lg-6 pb-5 pb-lg-0">
+                <img class="img-fluid w-100" src="{{ asset('client/img/testimonial.jpg') }}" alt="">
+            </div>
+            <div class="col-lg-6">
+                <h6 class="d-inline-block text-primary text-uppercase bg-light py-1 px-2">Feedback</h6>
+                <h1 class="mb-4">What Our Clients Say</h1>
+            </div>
+        </div>
+    </div>
+</div>
 @endif
 <!-- Testimonial End -->
